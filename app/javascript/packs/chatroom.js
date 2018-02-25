b7b7df9bbba0239326460b7d329afc7d5e2b3e09
Yves Trocheris <yves.trocheris@gmail.com>
2018-02-25 16:10:29 +0100

    coach & go v2

--- app/javascript/packs/chatroom.js
+++ app/javascript/packs/chatroom.js
@@ -1,8 +1,8 @@
 // Initialize an OpenTok Session object
-var session = OT.initSession(apiKey, sessionId);
+const session = OT.initSession(apiKey, sessionId);
 
 // Initialize a Publisher, and place it into the element with id="publisher"
-var publisher = OT.initPublisher('publisher', {
+const publisher = OT.initPublisher('publisher', {
     insertMode: 'append',
     width: '100%',
     height: '100%'
@@ -30,7 +30,7 @@ session.on({
   streamCreated: function(event) {
     // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
     // the element with id="subscribers"
-    var subContainer = document.createElement('div');
+    const subContainer = document.createElement('div');
     subContainer.id = 'stream-' + event.stream.streamId;
     document.getElementById('subscribers').appendChild(subContainer);
 
